db5d88a7b34fa06fe0b4c8a1dc04a5b7a89167ca
foreandr <foreandr@gmail.com>
2022-11-12 00:05:50 +0000

    databse change

--- mansura.py
+++ mansura.py
@@ -901,7 +901,7 @@ if __name__ == '__main__':
     #my_port = 443
     host = "0.0.0.0" #could be local host
     # logging.basicConfig(filename='app.log', encoding='utf-8', level=logging.DEBUG) #ISN'T    WORKING THE WAY I WANT
-    database.USER_FULL_RESET()
+    #database.USER_FULL_RESET()
     
     thread = Thread(target = distribution_algorithm.TESTING_TIMING, args = ())
     thread.start()
