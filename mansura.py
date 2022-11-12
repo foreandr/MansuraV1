14be6333229c8c498911d4433d587729554aea58
foreandr <foreandr@gmail.com>
2022-11-12 15:30:19 +0000

    commit?

--- mansura.py
+++ mansura.py
@@ -436,6 +436,7 @@ def user_profile_name(username):
         
         return render_template('post_details.html',
                             # POST DETAILS
+
                             len_text=len(og_post_text),
                             og_post_text=og_post_text,
                             og_post_img=og_post_img ,
@@ -901,7 +902,7 @@ if __name__ == '__main__':
     #my_port = 443
     host = "0.0.0.0" #could be local host
     # logging.basicConfig(filename='app.log', encoding='utf-8', level=logging.DEBUG) #ISN'T    WORKING THE WAY I WANT
-    database.USER_FULL_RESET()
+    # database.USER_FULL_RESET()
 
     
     thread = Thread(target = distribution_algorithm.TESTING_TIMING, args = ())
