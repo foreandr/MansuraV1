4495f1d53922a3d525cf634a87c79540774f1875
foreandr <foreandr@gmail.com>
2022-11-13 18:06:46 +0000

    got thread working?

--- mansura.py
+++ mansura.py
@@ -923,15 +923,17 @@ if __name__ == '__main__':
     #SUBTITLE Network Societey and it's Future
     
     #my_port = 443
-    host = "0.0.0.0" #could be local host
+    
+    #host = "0.0.0.0" #could be local host
     # logging.basicConfig(filename='app.log', encoding='utf-8', level=logging.DEBUG) #ISN'T    WORKING THE WAY I WANT
     # database.USER_FULL_RESET()
 
     
-    thread = Thread(target = distribution_algorithm.TESTING_TIMING, args = ())
-    thread.start()
+    #thread = Thread(target = distribution_algorithm.TESTING_TIMING, args = ())
+    #thread.start()
 
     #app.run(host=host, port="8080", debug=True, use_reloader=False)  # host is to get off localhost
-    serve(app, host=host)    
+    #serve(app, host=host)    
 
     # If the debugger is on, I can change my files in real time after saving
+    pass
